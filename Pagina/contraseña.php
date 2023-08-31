f1038eeb610ced6493ebabe14c14b4efb23cd950
DonIsaac117 <143756049+DonIsaac117@users.noreply.github.com>
2023-08-31 16:49:49 -0500

    Add files via upload

---
+++ Pagina/contraseña.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nueva-contraseña</title>
+    <link rel="stylesheet" href="estilo2.css">
+</head>
+<body>
+<div class="madre">
+    <!--PRIMERA CAJA-->
+        <div class="caja">
+            <h2 class="SENA">RECUPERAR SU CONTRASEÑA</h2>
+            <img class="imagen" src="Senalogo.png">
+        </div>
+
+        <!--FORMULARIO-->
+        <form method="post" name="signup-form">
+            <div class="form-element">
+                <label class="nc">INGRESE SU NUEVA CONTRASEÑA</label></br>
+                <input class="ncc" type="password" required="required" placeholder="">
+            </div>
+            <div class="form-element">
+                <label class="contraseña">CONFIRME LA CONTRASEÑA</label></br>
+                <input class="password" type="password" required="required" placeholder="">
+            </div>
+            </br><input class="button" type="submit" value="ENVIAR">   
+        </form>
+    </div>
+</body>
+</html>
---
+++ Pagina/login.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+// Obtengo los datos cargados en el formulario de login.
+$numero_documento = $_POST['ncc'];
+$password = $_POST['password'];
+
+// Datos para conectar a la base de datos.
+$nombreServidor = "localhost";
+$nombreUsuario = "root";
+$passwordBaseDeDatos = "";
+$nombreBaseDeDatos = "easygenz";
+
+// Crear conexión con la base de datos.
+$conn = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
+
+// Validar la conexión de base de datos.
+if ($conn ->connect_error) {
+    die("Connection failed: " . $conn ->connect_error);
+}
+
+// Consulta segura para evitar inyecciones SQL.
+
+
+
+// Verificando si el usuario existe en la base de datos.
+if($resultado){
+    // Guardo en la sesión el email del usuario.
+    $_SESSION['ncc'] = $documento;
+    
+    // Redirecciono al usuario a la página principal del sitio.
+    header("HTTP/1.1 302 Moved Temporarily"); 
+    header("Location: principal.php"); 
+}else{
+    echo 'El documento o contraseña es incorrecto, <a href="formulario.html">vuelva a intenarlo</a>.<br/>';
+}
+
+?>
