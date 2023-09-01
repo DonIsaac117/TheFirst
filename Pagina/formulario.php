415a0e1a9f46c2cc15ebc828bb789ff567722d58
DonIsaac117 <143756049+DonIsaac117@users.noreply.github.com>
2023-09-01 17:22:20 -0500

    Update formulario.php
    BUENO ESPERO ENTIENDAN XD

--- Pagina/formulario.php
+++ Pagina/formulario.php
@@ -1,44 +1,45 @@
-<?php
+<?php 
 
-session_start();
+$conex = mysqli_connect("localhost","root","","easygenz"); 
 
-// Obtengo los datos cargados en el formulario de login.
-$nombrecompleto = $_POST['username'];
-$numerodedocumento = $_POST['Numero de documento'];
-$Email = $_POST['email'];
-$contraseña = $_POST['Contrasena'];
-$ficha = $_POST['Numero de ficha'];
-$tipodesangre = $_POST['Tipo de sangre'];
-$nombrecompleto = $_POST['alergias'];
 
-// Datos para conectar a la base de datos.
-$nombreServidor = "localhost";
-$nombreUsuario = "root";
-$passwordBaseDeDatos = "";
-$nombreBaseDeDatos = "easygenz";
+if (isset($_POST['register'])) {
+    if (strlen($_POST['username']) >= 1 &&
+    strlen($_POST['Numero_documento']) >= 1 &&
+    strlen($_POST["email"]) >= 1 &&
+    strlen($_POST['Contraseña']) >= 1  &&
+    strlen($_POST['Numero_ficha']) >= 1  &&
+    strlen($_POST['Tipo_sangre']) >= 1 &&
+    strlen($_POST['alergias']) >= 1 ){
 
-// Crear conexión con la base de datos.
-$conn = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
+	    $username = trim($_POST['username']);
+	    $docu = trim($_POST['Numero_documento']);
+      $email = trim($_POST['email']);
+      $Contraseña = trim($_POST['Contraseña']);
+      $ficha = trim($_POST['Numero_ficha']);
+      $sangre = trim($_POST['Tipo_sangre']);
+      $alergias = trim($_POST['alergias']);
 
-// Validar la conexión de base de datos.
-if ($conn ->connect_error) {
-    die("Connection failed: " . $conn ->connect_error);
+	    $consulta = "INSERT INTO formulario(Nombre_completo, Numero_de_documento, Email, Contraseña, Numero_de_ficha, Tipo_de_sangre, alergias) VALUES ('$username','$docu','$email','$Contraseña','$ficha','$sangre','$alergias')";
+
+	    $resultado = mysqli_query($conex,$consulta);
+      
+	    if ($resultado) {
+	    	?> 
+	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
+           <?php
+	    } else {
+	    	?> 
+	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
+           <?php
+	    }
+    }   else {
+	    	?> 
+	    	<h3 class="bad">¡Por favor complete los campos!</h3>
+           <?php
+    }
 }
 
-// Consulta segura para evitar inyecciones SQL.
-
-
-
-// Verificando si el usuario existe en la base de datos.
-if($resultado){
-    // Guardo en la sesión el email del usuario.
-    $_SESSION['ncc'] = $documento;
-    
-    // Redirecciono al usuario a la página principal del sitio.
-    header("HTTP/1.1 302 Moved Temporarily"); 
-    header("Location: principal.php"); 
-}else{
-    echo 'El documento o contraseña es incorrecto, <a href="formulario.html">vuelva a intenarlo</a>.<br/>';
-}
+// TODO ESTO LO SAQUE DE https://www.youtube.com/watch?v=cka0J41iJY0 Y PUES LO MODIFIQUE EN BASE A NUETRA BASE DE DATOS OBVIAMENTE
 
 ?>
